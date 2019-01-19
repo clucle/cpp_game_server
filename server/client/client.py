2b259b96bb1cab14fe1dc58ac7832835608aedd9
정두진 <dude@jeongdujin-ui-MacBook-Pro.local>
2019-01-19 16:27:44 +0900

    feat: client recv msg
    Signed-off-by: 정두진 <dude@jeongdujin-ui-MacBook-Pro.local>

--- server/client/client.py
+++ server/client/client.py
@@ -1,16 +1,33 @@
-import sys
+# reference : http://parksk.tistory.com/112
+
 import socket
-import pygame
-import threading
-from pygame.locals import *
+import _thread
 
-
+HOST = "your server"
+PORT = "port"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("yout server", port))
+s.connect((HOST, PORT))
+
+
+def sending_msg():
+    while True:
+        data = input()
+        data = bytes(data, "utf-8")
+        s.send(data)
+    s.close()
+
+
+def getting_msg():
+    while True:
+        data = s.recv(1024)
+        data = str(data).split("b'", 1)[1].rsplit("'",1)[0]
+        print(data)
+    s.close()
+
+
+_thread.start_new_thread(sending_msg, ())
+_thread.start_new_thread(getting_msg, ())
+
 
 while True:
-    t = input()
-    if t == "exit":
-        break
-    else:
-        s.send("U".encode())
+    pass
