5e8a8152fb7af99b4f5a501ad4cd623bb691a415
clucle <wjdenwls123@gmail.com>
2019-02-17 13:54:19 +0900

    feat: load .env file for server host, port
    Signed-off-by: clucle <wjdenwls123@gmail.com>

---
+++ test/reconciliation/settings.py
@@ -0,0 +1,7 @@
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+SERVER_HOST = os.getenv("SERVER_HOST")
+SERVER_PORT = os.getenv("SERVER_PORT")
---
+++ test/reconciliation/socket.py
@@ -0,0 +1,8 @@
+# reference : http://parksk.tistory.com/112
+
+from reconciliation.settings import SERVER_HOST, SERVER_PORT
+
+HOST = SERVER_HOST
+PORT = SERVER_PORT
+
+print(HOST, PORT)
