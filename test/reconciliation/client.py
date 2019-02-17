3154c6dc9992c7435a04e3c3190bb5dae0aae4ae
clucle <wjdenwls123@gmail.com>
2019-02-17 11:47:24 +0900

    feat: add key event (left, right)
    Signed-off-by: clucle <wjdenwls123@gmail.com>

--- test/reconciliation/client.py
+++ test/reconciliation/client.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import random
 
 pygame.init()
@@ -11,6 +12,8 @@ def random_color():
 
 
 class Player:
+    X_DIFF = 40
+
     def __init__(self, x, screen):
         self.x = x
         self.y = 40
@@ -22,12 +25,13 @@ class Player:
     def draw(self):
         pygame.draw.circle(self.screen, self.color, [self.x, self.y], self.rad)
 
-    def move(self, x):
-        self.x = x
+    def move(self, x_diff):
+        self.x += x_diff
 
 
 # player list
-player_list = [Player(x=40, screen=screen)]
+player = Player(x=40, screen=screen)
+player_list = [player]
 
 # color
 white = (255, 255, 255)
@@ -36,7 +40,13 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_RIGHT:
+                player.move(Player.X_DIFF)
+            elif event.key == K_LEFT:
+                player.move(-Player.X_DIFF)
 
+    # DRAW
     screen.fill(white)
 
     for player in player_list:
